bcc7de63f020e52249151bae3685f20a2e92111d
Andre Peña <andrecrema10@gmail.com>
2026-06-04 00:14:47 -0500

    Add files via upload

---
+++ ProySisElectoral_BE/MesaBE.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProySisElectoral_BE
+{
+    public class MesaBE
+    {
+        public Int32 id_mesa { get; set; }
+        public String nombre { get; set; }
+        public String direccion { get; set; }
+    }
+}
---
+++ ProySisElectoral_BE/UsuarioBE.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ProySisElectoral_BE
+{
+    public class UsuarioBE
+    {
+        public Int32 id_usuario { get; set; }
+        public String dni { get; set; }
+        public String nombres { get; set; }
+        public String apellidos { get; set; }
+        public Int32 id_rol { get; set; }
+        public String direccion { get; set; }
+        public String sexo { get; set; }
+        public DateTime fecha_nacimiento { get; set; }
+        public String Id_Ubigeo { get; set; }
+    }
+}
