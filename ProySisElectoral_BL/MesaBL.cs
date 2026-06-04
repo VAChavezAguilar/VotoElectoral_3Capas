d39c78ca821d617504ee34de941a63bedaeee573
Andre Peña <andrecrema10@gmail.com>
2026-06-04 00:15:18 -0500

    Add files via upload

---
+++ ProySisElectoral_BL/MesaBL.cs
@@ -0,0 +1,12 @@
+﻿using System.Data;
+using ProySisElectoral_ADO;
+
+namespace ProySisElectoral_BL
+{
+    public class MesaBL
+    {
+        MesaADO objADO = new MesaADO();
+        public DataTable ListarMesa() { return objADO.ListarMesa(); }
+        public bool InsertarMesa(string nombre, string direccion) { return objADO.InsertarMesa(nombre, direccion); }
+    }
+}
---
+++ ProySisElectoral_BL/UsuarioBL.cs
@@ -0,0 +1,18 @@
+﻿using System.Data;
+using System;
+using ProySisElectoral_ADO;
+
+namespace ProySisElectoral_BL
+{
+    public class UsuarioBL
+    {
+        UsuarioADO objADO = new UsuarioADO();
+
+        public DataTable ListarUsuarios() { return objADO.ListarUsuarios(); }
+
+        public bool InsertarUsuario(string dni, string nombres, string apellidos, int id_rol, string direccion, string sexo, DateTime fecha_nac, string id_ubigeo)
+        {
+            return objADO.InsertarUsuario(dni, nombres, apellidos, id_rol, direccion, sexo, fecha_nac, id_ubigeo);
+        }
+    }
+} 
